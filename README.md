795070680832a4da29b8e9a00e84d39f0f61f26a
Stanislau Mikulich <42565957+stanislaumich@users.noreply.github.com>
2023-03-13 12:43:48 +0300

    AUTO FROM WORK 13.03.2023 12:43:48,07

---
+++ README.md
@@ -0,0 +1,1 @@
+"# KURS3LETO" 
---
+++ up to inet - push.bat
@@ -0,0 +1,8 @@
+set /p place=< s:\place.txt
+git add .
+git commit -m "AUTO FROM %place% %date% %time%"
+git config --global http.version HTTP/1.1
+git push
+rem git push origin --force
+git config --global http.version HTTP/2
+pause
